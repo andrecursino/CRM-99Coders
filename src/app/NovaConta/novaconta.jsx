a25f84c602ecf9faae6f0454ed5597cb2dadf6f7
André Cursino <andrephelipe@outlook.com>
2021-06-29 14:30:49 -0300

    Aula 11 - Criando novo usuário e Arrow Function (JS)

--- src/app/NovaConta/novaconta.jsx
+++ src/app/NovaConta/novaconta.jsx
@@ -1,23 +1,68 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
 import './novaconta.css';
 
+import firebase from '../Config/firebase'; 
+import 'firebase/auth';
+
 function NovaConta(){
+
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+  const [mensagem, setMensagem] = useState('');
+
+  function cadastrarUsuario(){
+    setMensagem('');
+
+    /*if (email.length === 0) {
+      setMensagem('Informe o email');
+      return;
+    }
+
+    if (senha.length === 0) {
+      setMensagem('Informe uma senha');
+      return;
+    }*/
+
+    if (!email || !senha) {
+      setMensagem('Informe todos os campos.');
+      return;
+    }
+
+    firebase.auth().createUserWithEmailAndPassword(email, senha).then(resultado =>{
+      alert('Usuário criado com sucesso');
+    }).catch(error => {
+      if (error.message === 'The email address is already in use by another account.') {
+        setMensagem('Endereço de email já utilizado.');
+      } else if (error.message === 'The email address is badly formatted.') {
+        setMensagem('Endereço de email mau formatado.');  
+      } else if (error.message === 'Password should be at least 6 characters') {
+        setMensagem('A senha deve contar pelo menos 6 caracteres.');
+      } else {
+        setMensagem('Erro ao criar conta: ' + error.message);
+      }  
+    })
+
+  }
+
     return <div className="d-flex align-items-center text-center form-container">
       <form className="form-signin">
         <img className="mb-4" src="/Images/logo-small2.png" alt=""/>
         <h1 className="h3 mb-3 fw-normal">Criar Conta</h1>
 
         <div className="form-floating">
-          <input type="email" className="form-control" id="floatingInput" placeholder="E-mail"/>
+          <input onChange={(e) => setEmail(e.target.value)} type="email" className="form-control" id="floatingInput" placeholder="E-mail"/>
           <label for="floatingInput">Email</label>
         </div>
         <div className="form-floating">
-          <input type="password" className="form-control" id="floatingPassword" placeholder="Senha"/>
+          <input onChange={(e) => setSenha(e.target.value)} type="password" className="form-control" id="floatingPassword" placeholder="Senha"/>
           <label for="floatingPassword">Senha</label>
         </div>
 
-        <button className="w-100 btn btn-lg btn-primary" type="submit">Criar Conta</button>
+        <button onClick={cadastrarUsuario} className="w-100 btn btn-lg btn-primary" type="button">Criar Conta</button>
+
+        {mensagem.length > 0 ? <div className="alert alert-danger mt-2" role="alert">{mensagem}</div> : null} 
+
 
         <div className="login-links mt-5">
           <Link to="/app" className="mx-3">Já tenho uma conta.</Link>
